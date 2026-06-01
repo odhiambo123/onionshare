3a0ead9d4fcfc59a18d45ffa5d6082a351d1a344
Saptak S <saptak013@gmail.com>
2026-06-01 15:49:26 +0530

    Updates version numbers to 2.6.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,6 @@
 project = "OnionShare"
 author = copyright = "Micah Lee, et al."
-version = release = "2.6.3"
+version = release = "2.6.4"
 
 extensions = ["sphinx_rtd_theme"]
 templates_path = ["_templates"]
@@ -22,7 +22,7 @@ languages = [
     ("Українська", "uk"),  # Ukrainian
 ]
 
-versions = ["2.3", "2.3.1", "2.3.2", "2.3.3", "2.4", "2.5", "2.6", "2.6.1", "2.6.2", "2.6.3"]
+versions = ["2.3", "2.3.1", "2.3.2", "2.3.3", "2.4", "2.5", "2.6", "2.6.1", "2.6.2", "2.6.3", "2.6.4"]
 
 html_theme = "sphinx_rtd_theme"
 html_logo = "_static/logo.png"
